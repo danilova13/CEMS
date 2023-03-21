d2995fec6e165d1355b7213c3d9c4ec80c0f6f21
Anna Danilova <annadanilova@macbook-air.home>
2023-03-21 17:45:25 -0400

    connected server to client on login

--- server/src/main/java/com/algoma/cems/controller/UserController.java
+++ server/src/main/java/com/algoma/cems/controller/UserController.java
@@ -1,5 +1,4 @@
 package com.algoma.cems.controller;
-
 import com.algoma.cems.model.User;
 import com.algoma.cems.model.UserLoginRequest;
 import com.algoma.cems.repository.UserRepository;
@@ -15,6 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin
 public class UserController {
     @Autowired
     private UserService userService;
