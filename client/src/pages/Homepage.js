9735b7b404023a091a7b399f17f6741d74b7b0af
A E <aelmollaz@gmail.com>
2023-03-22 21:36:41 +0200

    Try carousel component

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,10 +1,14 @@
-import Carousel from "../components/Carousel";
+import Carousel from "react-material-ui-carousel";
 
 const Homepage = () => {
   return (
     <div>
       <h1>Homepage</h1>
-      <Carousel></Carousel>
+      <Carousel>
+        {this.props.item.images.map((image, i) => (
+          <img key={i} src={image} />
+        ))}
+      </Carousel>
     </div>
   );
 };
