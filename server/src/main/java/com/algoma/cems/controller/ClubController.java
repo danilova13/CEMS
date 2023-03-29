a94c167efce5e921561327e7e58e2b4654543381
Cathy-Colette Tanya <cathycolette.tanya@gmail.com>
2023-03-29 15:10:11 -0400

    updated and edit club as well as get the events within clubs

--- server/src/main/java/com/algoma/cems/controller/ClubController.java
+++ server/src/main/java/com/algoma/cems/controller/ClubController.java
@@ -1,14 +1,17 @@
 package com.algoma.cems.controller;
 
 import com.algoma.cems.model.Club;
+import com.algoma.cems.model.Event;
 import com.algoma.cems.model.Financials;
 import com.algoma.cems.model.User;
+import com.algoma.cems.repository.ClubRepository;
 import com.algoma.cems.service.ClubService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @RestController
@@ -17,6 +20,9 @@ import java.util.Set;
 public class ClubController {
     @Autowired
     private ClubService clubService;
+
+    @Autowired
+    private ClubRepository clubRepository;
 
     @Autowired
     public ClubController(ClubService clubService){
@@ -34,6 +40,12 @@ public class ClubController {
     }
 
 
+    @PutMapping("/")
+    public Optional<Club> updateClub(@RequestBody Club updateClub){
+        clubRepository.save(updateClub);
+        return clubRepository.findById(updateClub.getIdClub());
+    }
+
     @GetMapping("/{idClub}")
     public Club getClub(@PathVariable int idClub){
         Club clubSelected = (Club)clubService.getClub(idClub);
@@ -44,6 +56,11 @@ public class ClubController {
     public ResponseEntity<String> deleteAllClubs(){
         clubService.deleteAllClubs();
         return ResponseEntity.ok("All clubs deleted");
+    }
+
+    @GetMapping("/{idClub}/events")
+    public Set<Event> listOfClubEvents(@PathVariable int idClub){
+        return clubService.getEventsForClub(idClub);
     }
     @GetMapping("/{idClub}/users")
     public Set<User> listOfUsers(@PathVariable int idClub){
